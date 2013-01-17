dc5031091ab61462a127d9d4f6da54663a2c7766
Rolando Velasco <rvelasco@fullsail.edu>
2013-01-17 01:25:21 -0800

    Updated html file; added variables, Procedure function and main flow to js file.

--- velasco_rolando_assignment2.js
+++ velasco_rolando_assignment2.js
@@ -1,1 +1,33 @@
-alert("JavaScript works!");
+// Rolando Velasco
+// 01-17-2013
+// Project 2 (Deliverable 1)
+// Theme: Knights
+
+// Set variables
+var knightName = "Sir Lot";
+var isEquipped = true;
+var roomName = "King Arthur's \"Round Table\"";
+var numOfArthurKnights = 7;
+var numOfSeats = 12;
+var willJoinArthurKnights = true;
+
+// Procedure function in flow chart
+// Check if the character has equipment and output appropriate message
+var checkEquipment = function(haveEquipment) {
+	if (haveEquipment === true) {
+		console.log("I see that you brought your own equipment. As to be expected from a hero in training!");
+	} else {
+		console.log("Since you came without any equipment we will provide you with some armor, a sword and shield, to get accustomed to the weight.");
+	};
+	return;
+};
+
+
+
+
+
+
+
+// Main flow
+console.log("Welcome, " + knightName + ", to the Heroes Training Camp.");
+checkEquipment(isEquipped);
