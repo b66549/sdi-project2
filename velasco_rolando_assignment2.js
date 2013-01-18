c070a6ee964324a42f0715f6e9feb663f8eefcf4
Rolando Velasco <rvelasco@fullsail.edu>
2013-01-17 16:47:08 -0800

    added Boolean function, updated main code and variables.

--- velasco_rolando_assignment2.js
+++ velasco_rolando_assignment2.js
@@ -6,13 +6,14 @@
 // Set variables
 var knightName = "Sir Lot";
 var isEquipped = true;
-var roomName = "King Arthur's \"Round Table\"";
+var wishToJoinArthurKnights = true;
+var curiousAboutJoiningArthurKnights = true;
 var numOfArthurKnights = 7;
 var numOfSeats = 12;
-var willJoinArthurKnights = true;
+var ArthurKnightsNames = [];
 
 // Procedure function in flow chart
-// Check if the character has equipment and output appropriate message
+// Check if the character has equipment and output the appropriate message
 var checkEquipment = function(haveEquipment) {
 	if (haveEquipment === true) {
 		console.log("I see that you brought your own equipment. As to be expected from a hero in training!");
@@ -22,12 +23,33 @@ var checkEquipment = function(haveEquipment) {
 	return;
 };
 
+// Boolean function in flow chart
+// Take two arguments, do a comparison conditional and output the appropriate message
+var canJoinArthurKnights = function(join, curious) {
+
+	// check if seats are available
+	var seatAvailable;
+	if (numOfArthurKnights < numOfSeats) {
+		seatAvailable = true;
+	} else {
+		seatAvaiable = false;
+	};
+	
+	// comparison conditional
+	if ( seatAvailable === true && (join === true || curious === true) ) {
+		console.log ("You're in luck!  The Round Table has room for a knight of your caliber.  And since you wish to be here, now is the perfect opportunity to test your skills.");
+		return true;
+	} else {
+		console.log ("Though it seems this is not the right time for you, consider this more of an educational experience.");
+		return false;
+	};
+};
 
 
 
 
 
-
-// Main flow
+// Main code
 console.log("Welcome, " + knightName + ", to the Heroes Training Camp.");
 checkEquipment(isEquipped);
+var willJoinArthurKnights = canJoinArthurKnights(wishToJoinArthurKnights, curiousAboutJoiningArthurKnights);
