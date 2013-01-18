71d9ac5939c11ec9783a2e7de0599e297eff1732
Rolando Velasco <rvelasco@fullsail.edu>
2013-01-17 20:03:38 -0800

    Added Array function, update main code and variables, updated summary in html file.

--- velasco_rolando_assignment2.js
+++ velasco_rolando_assignment2.js
@@ -1,6 +1,6 @@
 // Rolando Velasco
 // 01-17-2013
-// Project 2 (Deliverable 1)
+// Project 2
 // Theme: Knights
 
 // Set variables
@@ -12,7 +12,7 @@ var curiousAboutJoiningArthurKnights = true;
 var daysToTrain = 4;
 var numOfArthurKnights = 7;
 var numOfSeats = 12;
-var ArthurKnightsNames = [];
+var questDifficulty = [4, 3, 8, 4];
 
 // Procedure function in flow chart
 // Check if the character has equipment and output the appropriate message
@@ -68,14 +68,43 @@ var knightTitle = function (name, trait) {
 	var title = "";
 	title = name + ", the " + trait;
 	return title;
-}
+};
+
+// Array function in flow chart
+// Take an array and a number as arguments and use a for loop to do some math and outputs, then return an array.
+var determineQuests = function(difficulty, strength) {
+	var remainingStrength = strength;
+	var questCompletion =[];
+	for (var counter = 0; counter < difficulty.length; counter++) {
+		if (remainingStrength > difficulty[counter]) {
+			questCompletion[counter] = "success";
+			remainingStrength = remainingStrength - difficulty[counter];
+			console.log("Based on your strength from training, you will be able to complete Quest #" + (counter+1) + ". You will have less strength for the next quest.");
+		} else {
+			questCompletion[counter] = "need more training";
+			console.log("Based on your strength from training, you will be too weak to complete Quest #" + (counter+1) + ". You will need more training.");
+		};
+	};
+	return questCompletion;
+};
 
 
 
 // Main code
-console.log("Welcome, " + knightName + ", to the Heroes Training Camp.");
+console.log("Welcome, " + knightName + ", to the Heroes Training Camp.  Here we put you through rigorous training to gauge your potential.");
 checkEquipment(isEquipped);
 var willJoinArthurKnights = canJoinArthurKnights(wishToJoinArthurKnights, curiousAboutJoiningArthurKnights);
 console.log("You will be given " + daysToTrain + " days to train here.  Training begins now!");
 var newStrength = training(daysToTrain);
 var newTitle = knightTitle(knightName, knightTrait);
+var quests = determineQuests(questDifficulty, newStrength);
+
+// final output with returned values
+console.log("After careful evaluation of your training, this is what was determined:");
+console.log("It is indeed " + willJoinArthurKnights + " that you will join King Arthur and become one of his knights.");
+console.log("If you were to take part, your full name will become \"" + newTitle + "\".");
+console.log("Having trained for " + daysToTrain + " days, your strength is evaluated at " + newStrength + ".");
+console.log("We have compiled a list of " + quests.length + " quests and determined what you are capable of:");
+console.log(quests);
+console.log("Your potential has been measured.  What you do now is up to you.");
+
